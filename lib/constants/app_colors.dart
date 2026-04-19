f36d5c8391675769c603ff85e916fc36749d4447
takashi0602 <takashi@takashimelon.com>
2026-04-19 22:07:58 +0900

    chore: カラーの定義

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const primary = Color(0xFF9F80BE);
+  static const background = Color(0xFFEEE8D7);
+  static const textPrimary = Color(0xFF343434);
+}
