50721724d4376c0756b75f75654d9dd8b199172f
takashi0602 <takashi@takashimelon.com>
2026-04-19 22:27:20 +0900

    feat: トップページの作成

---
+++ lib/pages/top_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:xen2/components/primary_button.dart';
+import 'package:xen2/constants/app_colors.dart';
+
+class TopPage extends StatelessWidget {
+  const TopPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            const Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  'Xen',
+                  style: TextStyle(fontSize: 48, height: 1, letterSpacing: 10),
+                ),
+                Text('Extreme Xen', style: TextStyle(fontSize: 14, height: 1)),
+              ],
+            ),
+            SizedBox(
+              width: 250,
+              child: PrimaryButton(
+                label: '禅',
+                onPressed: () {
+                  // Navigator.pushReplacement(
+                  //   context,
+                  //   MaterialPageRoute(builder: (_) => const PlayPage()),
+                  // );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
